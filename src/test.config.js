dc3ccbb35cfe7ef068c5f14c4bf551f4b12e7c65
Bill42362 <bill42362@gmail.com>
2017-08-14 22:24:23 +0800

    Enable set express response headers in handlers and enable cors.

--- src/test.config.js
+++ src/test.config.js
@@ -2,5 +2,5 @@
 'use strict';
 
 const LOCAL_BASE_URL = `http://localhost:${process.env.PORT || '3000'}`;
-const AWS_BASE_URL = 'https://h15xzbmjfh.execute-api.ap-northeast-2.amazonaws.com/dev';
+const AWS_BASE_URL = 'https://489qcrwwfh.execute-api.ap-northeast-2.amazonaws.com/dev';
 export const BASE_URL = 'aws' === process.env.NODE_ENV ? AWS_BASE_URL : LOCAL_BASE_URL;
